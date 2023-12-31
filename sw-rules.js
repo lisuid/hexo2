038c9bd33355e66f72c0aa96037386ed9ab6af84
lisuid/lisuid1.github.io <3238355213@qq.com>
2023-12-31 16:23:37 +0800

    up

--- sw-rules.js
+++ sw-rules.js
@@ -1,128 +1,143 @@
-/**
- * @see https://kmar.top/posts/b70ec88f/
- */
-
 module.exports.config = {
-  /**
-   * 与 ServiceWorker 有关的配置项
-   * 若想禁止插件自动生成 sw，此项填 false 即可
-   * @type ?Object|boolean
-   */
-  serviceWorker: {
-    escape: 0,
-    cacheName: "AnZhiYuThemeCache",
-    debug: false,
-  },
-  register: {
-    onsuccess: undefined,
-    onerror: undefined,
-    builder: (root, hexoConfig, pluginConfig) => {
-      const { onerror, onsuccess } = pluginConfig.register;
-      return `<script>
-                    (() => {
-                        const sw = navigator.serviceWorker
-                        const error = ${onerror && onerror.toString()}
-                        if (!sw?.register('${new URL(root).pathname}sw.js')
-                            ${onsuccess ? "?.then(" + onsuccess + ")" : ""}
-                            ?.catch(error)
-                            ) error()
-                    })()
-                </script>`;
+    /** @type {?ServiceWorkerConfig|boolean} */
+    serviceWorker: {
+        /** @type {number} */
+        escape: 0,
+        /** @type {string} */
+        cacheName: 'SolitudeCache',
+        /** @type {Boolean} */
+        debug: false
     },
-  },
-  dom: {
-    onsuccess: () => {
-      caches.match('https://id.v3/').then(function(response) {
-        if (response) {
-          // 如果找到了匹配的缓存响应
-          response.json().then(function(data) {
-            anzhiyuPopupManager && anzhiyuPopupManager.enqueuePopup('通知📢', `已刷新缓存，更新为${data.global + "." + data.local}版本最新内容`, null, 5000);
-          });
-        } else {
-          console.info('未找到匹配的缓存响应');
+    /** @type {?RegisterConfig|boolean} */
+    register: {
+        /** @type {?VoidFunction} */
+        onsuccess: undefined,
+        /** @type {?VoidFunction} */
+        onerror: () => console.error('Service Worker 注册失败！可能是由于您的浏览器不支持该功能！'),
+        /**
+         * @param root {string} 网页根目录的 URL
+         * @param framework {Object} 框架对象
+         * @param pluginConfig {SwppConfig} swpp 配置项
+         * @return {string} 一个 HTML 标签的字符串形式
+         */
+        builder: (root, framework, pluginConfig) => {
+            const {onerror, onsuccess} = pluginConfig.register;
+            return `
+            <script>
+                (() => {
+                    const sw = navigator.serviceWorker;
+                    const error = ${onerror && onerror.toString()};
+                    if (!sw?.register('${new URL(root).pathname}sw.js')
+                        ${onsuccess ? `?.then(${onsuccess.toString()})` : ''}
+                        ?.catch(error)
+                    ) error()
+                })()
+            </script>`;
         }
-      }).catch(function(error) {
-        console.error('缓存匹配出错:', error);
-      });
     },
-  },
-  json: {
-    maxHtml: 15,
-    charLimit: 1024,
-    merge: ['page', 'archives', 'categories', 'tags'],
-    exclude: {
-      localhost: [],
-      other: [],
+    /** @type {?DomConfig|boolean} */
+    dom: {
+        /** @type {?VoidFunction} */
+        onsuccess: () => {
+            caches.match(location.href).then(res => {
+                if (res)
+                    res.json().then(json => {
+                        utils && utils.snackbarShow(`已刷新缓存，更新为${json.global + '.' + json.local}版本最新内容`, false, 2000)
+                    })
+                else
+                    console.info('未找到缓存')
+            }).catch((error) => console.error("缓存匹配出错", error))
+        }
     },
-  },
-  external: {
-    timeout: 5000,
-    js: [],
-    stable: [
-      /^https:\/\/npm\.elemecdn\.com\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
-      /^https:\/\/cdn\.cbd\.int\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
-      /^https:\/\/cdn\.jsdelivr\.net\/npm\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
-    ],
-    replacer: srcUrl => {
-      if (srcUrl.startsWith('https://npm.elemecdn.com')) {
-        const url = new URL(srcUrl)
-        return [
-            srcUrl,
-            `https://cdn.cbd.int` + url.pathname,
-            `https://cdn.jsdelivr.net/npm` + url.pathname,
-            `https://cdn1.tianli0.top/npm` + url.pathname,
-            `https://fastly.jsdelivr.net/npm` + url.pathname
-        ]
-      } else {
-        return srcUrl
-      }
+    /** @type {?VersionJsonConfig|boolean} */
+    json: {
+        /** @type {number} */
+        maxHtml: 15,
+        /** @type {number} */
+        charLimit: 1024,
+        /** @type {string[]} */
+        merge: [],
+        exclude: {
+            /** @type {RegExp[]} */
+            localhost: [],
+            /** @type {RegExp[]} */
+            other: []
+        }
     },
-  }
+    /** @type {?ExternalMonitorConfig|boolean} */
+    external: {
+        /** @type {number} */
+        timeout: 5000,
+        /** 拉取文件时地并发限制 */
+        concurrencyLimit: 100,
+        /** @type {({head: string, tail: string}|function(string):string[])[]} */
+        js: [],
+        /** @type {RegExp[]} */
+        stable: [
+            /^https:\/\/npm\.elemecdn\.com\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
+            /^https:\/\/cdn\.cbd\.int\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
+            /^https:\/\/cdn\.jsdelivr\.net\/npm\/[^/@]+\@[^/@]+\/[^/]+\/[^/]+$/,
+        ],
+        /**
+         * @param srcUrl {string} 原始 URL
+         * @return {string[]|string}
+         */
+        replacer: srcUrl => {
+            if (srcUrl.startsWith('https://cdn.jsdelivr.net/npm/')) {
+                const pathname = new URL(srcUrl).pathname;
+                return [
+                    srcUrl,
+                    `https://cdn.cbd.int/${pathname}`,
+                    `https://npm.elemecdn.com/${pathname}`,
+                    `https://fastly.jsdelivr.net/npm/${pathname}`,
+                    `https://cdn1.tianli0.top/npm/${pathname}`
+                ];
+            } else {
+                return srcUrl;
+            }
+        }
+    }
 };
 
 /**
  * 缓存列表
- * @param clean 清理全站时是否删除其缓存
- * @param match {function(URL)} 匹配规则
+ * @param {Boolean} clean 是否清空缓存
+ * @param {function(URL)} match 匹配缓存的函数
  */
 module.exports.cacheRules = {
-  simple: {
-    clean: true,
-    search: false,
-    match: (url, $eject) => {
-      const allowedHost = $eject.domain;
-      const allowedPaths = ["/404.html", "/css/index.css"];
-      return url.host === allowedHost && allowedPaths.includes(url.pathname);
+    simple: {
+        clean: false,
+        search: false,
+        match: (url,$eject) => url.host === $eject.domain && ["/404.html", "/css/index.css"].includes(url.pathname)
     },
-  },
-  cdn: {
-    clean: true,
-    match: url =>
-      [
-        "cdn.cbd.int",
-        "lf26-cdn-tos.bytecdntp.com",
-        "lf6-cdn-tos.bytecdntp.com",
-        "lf3-cdn-tos.bytecdntp.com",
-        "lf9-cdn-tos.bytecdntp.com",
-        "cdn.staticfile.org",
-        "npm.elemecdn.com",
-      ].includes(url.host) && url.pathname.match(/\.(js|css|woff2|woff|ttf|cur)$/),
-  },
+    cdn: {
+        clean: true,
+        match: url => [
+            "cdn.cbd.int",
+            "lf26-cdn-tos.bytecdntp.com",
+            "lf6-cdn-tos.bytecdntp.com",
+            "lf3-cdn-tos.bytecdntp.com",
+            "lf9-cdn-tos.bytecdntp.com",
+            "cdn.staticfile.org",
+            "npm.elemecdn.com",
+        ].includes(url.host) && url.pathname.match(/\.(js|css|woff2|woff|ttf|cur)$/),
+
+    }
 };
 
 /**
  * 获取一个 URL 对应的备用 URL 列表，访问顺序按列表顺序，所有 URL 访问时参数一致
  * @param srcUrl {string} 原始 URL
- * @return {{list: string[], timeout: number}} 返回 null 或不返回表示对该 URL 不启用该功能。timeout 为超时时间（ms），list 为 URL 列表，列表不包含原始 URL 表示去除原始访问
+ * @return {list: string[], timeout: number} 返回 null 或不返回表示对该 URL 不启用该功能。timeout 为超时时间（ms），list 为 URL 列表，列表不包含原始 URL 表示去除原始访问
  */
 module.exports.getSpareUrls = srcUrl => {
-  if (srcUrl.startsWith("https://npm.elemecdn.com")) {
-    return {
-      timeout: 3000,
-      list: [srcUrl, `https://cdn.cbd.int/${new URL(srcUrl).pathname}`],
-    };
-  }
-};
+    if (srcUrl.startsWith("https://npm.elemecdn.com")) {
+        return {
+            timeout: 3000,
+            list: [srcUrl, `https://cdn.cbd.int/${new URL(srcUrl).pathname}`],
+        };
+    }
+}
 
 /**
  * 获取要插入到 sw 中的变量或常量
@@ -131,10 +146,10 @@ module.exports.getSpareUrls = srcUrl => {
  * @return {Object} 要插入的键值对
  */
 module.exports.ejectValues = (hexo, rules) => {
-  return {
-    domain: {
-      prefix: "const",
-      value: new URL(hexo.config.url).host,
-    },
-  };
+    return {
+        domain: {
+            prefix: "const",
+            value: new URL(hexo.config.url).host,
+        },
+    };
 };
