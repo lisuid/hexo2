409ab738de9b7430baf430c57a0bb2c5100c599c
lisuid2 <2150844928@qq.com>
2024-01-06 14:29:07 +0800

    1.6

--- hexo-offline.config.cjs
+++ hexo-offline.config.cjs
@@ -1,7 +1,7 @@
 // offline config passed to workbox-build.
 module.exports = {
 	// 静态文件合集，如果你的站点使用了例如 webp 格式的文件，请将文件类型添加进去。
-	globPatterns: ["**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}"],
+	globPatterns: ["**/*.{js,html,css,png,jpg,webp,gif,svg,eot,ttf,woff}"],
 	globDirectory: "public",
 	swDest: "public/service-worker.js",
 	maximumFileSizeToCacheInBytes: 10485760, // 缓存的最大文件大小，以字节为单位。
